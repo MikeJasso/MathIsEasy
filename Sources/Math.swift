e957a96409c561fe9ac43ad3f7156acb09a1f022
Miguel Angel Jasso Estrada <miguel.jasso@payclip.com>
2020-02-26 12:35:26 -0600

    add sub, mul, div

--- Sources/Math.swift
+++ Sources/Math.swift
@@ -14,4 +14,17 @@ public class Math: NSObject {
     public func sum(a: Double, b: Double) -> Double{
         return a+b
     }
+    
+    public func sub(a: Double, b: Double) -> Double{
+        return a-b
+    }
+    
+    public func mul(a: Double, b: Double) -> Double{
+        return a*b
+    }
+    
+    public func div(a: Double, b: Double) -> Double{
+        return a/b
+    }
+    
 }
